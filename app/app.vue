7806e54f29460b11141faca344bc003e64988708
cbrunie <brunie.clement@gmail.com>
2025-09-03 11:18:12 +0200

    refactor: remove color mode

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
-
-const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
-
 useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color }
+    { key: 'theme-color', name: 'theme-color', content: '#050505' }
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' }
